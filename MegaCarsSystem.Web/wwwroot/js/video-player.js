08c58d9491f4cf5ef9fa4b2da5b9e8a15b71874a
Vladimir Shalamanov <vladi.gesh@gmail.com>
2023-07-28 20:02:02 +0300

    Setuped functionality and some logic for video in home page.

---
+++ MegaCarsSystem.Web/wwwroot/js/video-player.js
@@ -0,0 +1,25 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var videoPlayer = document.getElementById('videoPlayer');
+    var videoElement = videoPlayer.children[0];
+    var videos = ['/videos/video1.mp4', '/videos/video2.mp4', '/videos/video3.mp4'];
+    var currentVideoIndex = 0;
+
+    videoPlayer.addEventListener('ended', function () {
+        if (currentVideoIndex < videos.length - 1) {
+            currentVideoIndex++;
+            videoElement.setAttribute("src", videos[currentVideoIndex]);
+
+            videoPlayer.load(); 
+            videoPlayer.play();
+        }
+        else {
+            // All videos have played; you can choose to loop or stop here
+            // For example, to loop back to the first video, use:
+            currentVideoIndex = 0;
+            videoElement.setAttribute("src", videos[currentVideoIndex]);
+
+            videoPlayer.load();
+            videoPlayer.play();
+        }
+    });
+});
