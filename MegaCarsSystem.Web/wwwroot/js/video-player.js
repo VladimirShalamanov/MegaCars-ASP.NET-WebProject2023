cef07d8519337b8470212c2682a109b92af784e7
Vladimir Shalamanov <vladi.gesh@gmail.com>
2023-07-30 20:29:50 +0300

    The initial logic for the home page. Edited is the second video.

--- MegaCarsSystem.Web/wwwroot/js/video-player.js
+++ MegaCarsSystem.Web/wwwroot/js/video-player.js
@@ -1,23 +1,23 @@
-﻿document.addEventListener('DOMContentLoaded', function () {
+﻿//document.addEventListener('DOMContentLoaded', function () {
 
-    var videos = ['/videos/video1.mp4', '/videos/video2.mp4', '/videos/video3.mp4'];
-    var currentVideoIndex = 0;
+//    var videos = ['/videos/video1.mp4', '/videos/video2.mp4', '/videos/video3.mp4'];
+//    var currentVideoIndex = 0;
 
-    var videoPlayer = document.getElementById('videoPlayer');
-    var videoElement = videoPlayer.children[0];
+//    var videoPlayer = document.getElementById('videoPlayer');
+//    var videoElement = videoPlayer.children[0];
 
-    videoPlayer.addEventListener('ended', function () {
+//    videoPlayer.addEventListener('ended', function () {
 
-        if (currentVideoIndex < videos.length - 1) {
-            currentVideoIndex++;
-        }
-        else {
-            currentVideoIndex = 0;
-        }
+//        if (currentVideoIndex < videos.length - 1) {
+//            currentVideoIndex++;
+//        }
+//        else {
+//            currentVideoIndex = 0;
+//        }
 
-        videoElement.setAttribute("src", videos[currentVideoIndex]);
+//        videoElement.setAttribute("src", videos[currentVideoIndex]);
 
-        videoPlayer.load();
-        videoPlayer.play();
-    });
-});
+//        videoPlayer.load();
+//        videoPlayer.play();
+//    });
+//});
